0d27fc7920967d4025299b216b94c9ed003c9e6f
dungp-dsai <dungpham290198@gmail.com>
2026-03-14 14:43:55 +0700

    add navigation stack, link and tab bar

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,13 @@
+import { Link } from 'expo-router';
 import { StyleSheet, Text, View } from 'react-native';
-
 
 export default function Index() {
   return(
     <View style={styles.container}>
       <Text style={styles.text}>Home Screen</Text>
+      <Link href="/about" style={styles.button}>
+        Go to About screen
+      </Link>
     </View>
   );
 }
@@ -20,6 +23,11 @@ const styles = StyleSheet.create(
     },
     text: {
       color: '#fff'
+    }, 
+    button: {
+      fontSize: 20,
+      textDecorationLine: 'underline',
+      color: '#fff',
     }
   }
 );
